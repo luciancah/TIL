2939584e77a21a31bb262a1eb15f62a73fb836aa
Jinhyung Lee <luciancah@gmail.com>
2024-08-06 15:17:42 +0900

    [level 2] Title: 프로세스, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,12 @@
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+            
